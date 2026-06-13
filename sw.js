505a4df71aae8e2e1ae23197cd062fd812a7913d
oloffasten-wq <olof.fasten@gmail.com>
2026-06-13 12:29:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  caches.keys().then(keys => keys.forEach(key => caches.delete(key)));
+  self.registration.unregister();
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.navigate(client.url));
+  });
+});
+ 
